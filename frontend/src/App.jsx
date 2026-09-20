a89726fad8a535c8f7e9e419805e9ba9c3c9e6d5
Noori <noorzafar882@gmail.com>
2026-09-20 11:54:12 +0500

    Remove BuyerProducts page and its associated route from the application

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,7 +12,7 @@ import ProtectedRoutes from "./routes/ProtectedRoutes.jsx";
 import ProtectedLayout from "./layout/ProtectedLayout.jsx";
 import AddProduct from "./pages/addProduct.jsx";
 import MyProducts from "./pages/MyProducts.jsx";
-import BuyerProducts from "./pages/BuyerProducts";
+
 function App() {
   return (
     <>
@@ -31,7 +31,6 @@ function App() {
 
             <Route element={<ProtectedRoutes allowedRoles={["buyer"]} />}>
               <Route path="/buyer-dashboard" element={<BuyerDashboard />} />
-              <Route path="/buyer-products" element={<BuyerProducts />} />
             </Route>
 
             <Route element={<ProtectedRoutes allowedRoles={["farmer"]} />}>
